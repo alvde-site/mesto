cb0ebd05648dbc2e4d2eaed98cf78ecdaee3eb19
Aleksandr Demidenko <sashatro@mail.ru>
2022-01-14 19:54:13 +0300

    Добавить script

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close');
+
+
+
+function popupOpen () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
+}
+
+function popupClose () {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', popupOpen);
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', popupClose);
