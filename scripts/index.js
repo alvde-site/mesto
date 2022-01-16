dd28eb0b96bc5bbe77b5acb495a0c670112fd319
Aleksandr Demidenko <sashatro@mail.ru>
2022-01-16 21:42:40 +0300

    style: Добовить transition и cursor для button, переместил фото из blocks в images

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,22 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close');
 
 
-
+//Открытие popup
 function popupOpen () {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+// Обработчик «отправки» формы
 function formSubmitHandler (evt) {
-    evt.preventDefault();
+    evt.preventDefault(); // отмена стандартной отправки формы.
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupClose();
 }
 
+// Закрытие popup
 function popupClose () {
   popup.classList.remove('popup_opened');
 }
