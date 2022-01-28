025e49480b6a8674fc0984eba8f878837ca9b633
Aleksandr Demidenko <sashatro@mail.ru>
2022-01-28 17:49:04 +0300

    Добавить карточки с местами  с помощью js на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,53 @@ let jobInput = document.querySelector('.form__input_profile_job');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close');
 
+//Создание карточек
+
+const template = document.querySelector('#element_template').content;
+const elements = document.querySelector('.elements__container');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+function render () {
+  initialCards.forEach(renderElement);
+}
+
+function renderElement(item) {
+  const newElement = template.cloneNode(true);
+  newElement.querySelector('.element__description-text').innerText = item.name;
+  newElement.querySelector('.element__img').src = item.link;
+  elements.appendChild(newElement);
+}
+
+render();
 
 //Открытие popup
+
 function popupOpen () {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
