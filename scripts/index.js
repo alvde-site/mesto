79c3ddd3e9b075929c574df851dc9ab363ee8e31
Aleksandr Demidenko <sashatro@mail.ru>
2022-02-02 09:52:22 +0300

    Добавить обработчик для submit edetForm

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,21 @@ const profileName = document.querySelector('.profile__name'); // Имя проф
 const profileJob = document.querySelector('.profile__job');  // Профессия профиля
 const editButton = document.querySelector('.profile__edit-button'); // Кнопка открытия popup формы заполнения профиля
 const popupEditForm = document.querySelector('.popup_handle_profile');  // Popup форма заполнения профиля данными от пользователя
+const popupEditCloseButton = popupEditForm.querySelector('.popup__close');
+const popupEditFormElement = popupEditForm.querySelector('.form');
 const nameInput = document.querySelector('.form__input_profile_name');
 const jobInput = document.querySelector('.form__input_profile_job');
+const popupEditFormSubmit = popupEditForm.querySelector('.form__submit_edit-form');
+popupEditFormSubmit.addEventListener('click', submitProfileForm);
 const addButton = document.querySelector('.profile__add-button'); // Кнопка открытия popup формы добавления катрочки
 const popupAddForm = document.querySelector('.popup_handle_add-element'); // Popup форма создания карточки данными от пользователя
+const popupAddCloseButton = popupAddForm.querySelector('.popup__close');
+const popupAddFormElement = popupAddForm.querySelector('.form');
 const addElementName = document.querySelector('.form__input_add_name');
 const addElementLink = document.querySelector('.form__input_add_link');
+const popupAddFormSubmit = popupAddForm.querySelector('.form__submit_add-form');
 const popupImageViewing = document.querySelector('.popup_handle_image-viewing'); // Popup просмотр изображения
+const closeButton = popupImageViewing.querySelector('.image-viewing__close');
 const popupImage = document.querySelector('.image-viewing__image');
 const popupCaption = document.querySelector('.image-viewing__caption');
 
@@ -71,7 +79,7 @@ function addCardContent(name, link, data) {
 }
 
 function addUserCardContent(name, link) {
-  name.innerText =  addElementName.value;
+  name.innerText = addElementName.value;
   link.src = addElementLink.value;
   name.alt = addElementName.value;
 }
@@ -84,9 +92,7 @@ function createCard(item) {
   const imageCaption = newElement.querySelector('.element__description-text'); // Название карточки
 
   addCardContent(imageCaption, imageSrc, item);
-
   addListenerToElement(likeButton, removeButton, imageSrc);
-
   renderElement(newElement, elements);
 }
 
@@ -101,6 +107,7 @@ function setElementContent() {
   const imageSrc = newElement.querySelector('.element__img');
   const likeButton = newElement.querySelector('.element__like-button');
   const imageCaption = newElement.querySelector('.element__description-text');
+
   imageCaption.innerText =  addElementName.value;
   imageSrc.src = addElementLink.value;
   imageSrc.alt = addElementName.value;
@@ -108,7 +115,10 @@ function setElementContent() {
   addUserCardContent(imageCaption, imageSrc);
   addListenerToElement(likeButton, removeButton, imageSrc);
   elements.prepend(newElement);
+  formSubmitHandler (popupAddFormSubmit);
 }
+
+popupAddFormSubmit.addEventListener('click', setElementContent);
 
 
 // Переключение класса у like кнопки
@@ -129,17 +139,19 @@ function addListenterToCloseButton(button) {
   button.addEventListener('click', closePopup);
 }
 
+addListenterToCloseButton(closeButton);
+addListenterToCloseButton(popupEditCloseButton);
+addListenterToCloseButton(popupAddCloseButton);
+
 // Просмотр изображения
 
 function viewImage(img) {
   const imageSrc = img.target.getAttribute('src');
   const imageCaption = img.target.closest('.element').querySelector('.element__description-text').textContent;
-  const closeButton = popupImageViewing.querySelector('.image-viewing__close');
 
   popupImage.setAttribute('src', imageSrc);
   popupCaption.innerText = imageCaption;
   openPopup(popupImageViewing);
-  addListenterToCloseButton(closeButton);
 }
 
 // Добавление слушателя при submit формам
@@ -155,28 +167,22 @@ function setProfileText() {
   profileJob.textContent = jobInput.value;
 }
 
-function openPopupEditForm () {
-  const closeButton = popupEditForm.querySelector('.popup__close');
-  const formElement = popupEditForm.querySelector('.form');
+popupEditFormElement.addEventListener('submit', setProfileText);
 
+function submitProfileForm(evt) {
+  formSubmitHandler(evt);
+  setProfileText();
+}
+
+function openPopupEditForm () {
   openPopup(popupEditForm);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-
-  addListenterToCloseButton(closeButton);
-  addListenerToSubmitForm(formElement);
-  formElement.addEventListener('submit', setProfileText)
 }
 
 function openPopupAddElementForm () {
-  const closeButton = popupAddForm.querySelector('.popup__close');
-  const formElement = popupAddForm.querySelector('.form');
-
   openPopup(popupAddForm);
-
   addListenterToCloseButton(closeButton);
-  addListenerToSubmitForm(formElement);
-  formElement.addEventListener('submit', setElementContent);
 }
 
 // Закрыть popups
