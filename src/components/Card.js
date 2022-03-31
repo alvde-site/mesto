522931a7459134b133ff9e990d30accd1ddbe96f
Aleksandr Demidenko <sashatro@mail.ru>
2022-03-31 12:24:54 +0300

    feat:Отображать колическтво лайков на карточке

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ export default class Card {
   constructor(data, cardSelector, handleCardClick) {
     this._name = data.name;
     this._link = data.link;
+    this._likes = data.likes;
     this._template = cardSelector;
     this._handleCardClick = handleCardClick;
   }
@@ -21,12 +22,14 @@ export default class Card {
     this._cardText = this._element.querySelector('.element__description-text');
     this._likeButton = this._element.querySelector('.element__like-button');
     this._removeButton = this._element.querySelector('.element__remove-button');
+    this._likeCount = this._element.querySelector('.element__like-count');
 
     this._setEventListenter();
 
     this._cardText.innerText = this._name;
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
+    this._likeCount.innerText = this._likes.length;
     return this._element;
   }
 
