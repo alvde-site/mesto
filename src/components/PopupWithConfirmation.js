9bc169849597cfd11b8a0c82f3cb82b405cf49ec
Aleksandr Demidenko <sashatro@mail.ru>
2022-04-04 23:39:41 +0300

    feat:Поправить PopupWithConfirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,30 +1,18 @@
 import Popup from './Popup.js';
 
-export default class PopupWithConfirmation extends Popup {
+export default class PopupWithForm extends Popup {
   constructor({ popupSelector, submitForm }) {
     super(popupSelector);
     this._submitForm = submitForm;
     this._popupForm = this._popupElement.querySelector('.form');
-    this._inputLists = this._popupElement.querySelectorAll('.form__input');
-  }
-
-  _getInputValues() {
-    this._formValues = {};
-    this._inputLists.forEach(input => {
-      this._formValues[input.name] = input.value;
-    });
-    return this._formValues;
   }
 
   setEventListeners() {
     this._popupForm.addEventListener('submit', (evt) => {
       evt.preventDefault();
+      this._submitForm();
     });
     super.setEventListeners();
-  }
-
-  close() {
-    super.close();
   }
 
   confirmDeleteCard(newSubmitForm) {
