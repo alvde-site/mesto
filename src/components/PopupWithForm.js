017c657457fa44075a4e83be75071f0ce9202a23
Aleksandr Demidenko <sashatro@mail.ru>
2022-04-07 19:40:46 +0300

    refactor:Доработать кнопку Сохранение...

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,6 +7,7 @@ export default class PopupWithForm extends Popup {
     this._popupForm = this._popupElement.querySelector('.form');
     this._inputLists = this._popupElement.querySelectorAll('.form__input');
     this._submitButton = this._popupElement.querySelector('.form__submit');
+    this._initialText = this._submitButton.textContent;
   }
 
   _getInputValues() {
@@ -39,7 +40,7 @@ export default class PopupWithForm extends Popup {
     if(isLoading) {
       this._submitButton.textContent = 'Сохранение...';
     } else {
-      this._submitButton.textContent = 'Сохранить';
+      this._submitButton.textContent = this._initialText;
     }
   }
 }
